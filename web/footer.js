e8ade3741880bb37f15c704d428888db5440cf02
FABY_ <faby_star_97@hotmail.it>
2020-08-31 16:26:36 +0200

    Footer.js

---
+++ web/footer.js
@@ -0,0 +1,34 @@
+Vue.component('my-footer', {
+    template: '<footer class="site-footer page-footer">\n' +
+        '    <div class="container">\n' +
+        '        <div class="row">\n' +
+        '            <div class="col l3 s12 hide-on-med-and-down"  >\n' +
+        '               <img src="https://upload.wikimedia.org/wikipedia/it/thumb/4/47/Unito-logo.svg/115px-Unito-logo.svg.png" class="immagine-footer"> \n' +
+        '            </div> \n'+
+        '            <div class="col l7 s12" >\n' +
+
+        '            </div>\n' +
+        '            <div class="col l7 s12" >\n' +
+
+        '            </div>\n' +
+        '            <div class="col l6 s12" >\n' +
+        '                <h5 class="white-text"> StudiaOnline.com</h5>\n' +
+        '                <p class="grey-text text-lighten-4">Università degli Studi di Torino - Via Verdi, 8 - 10124 Torino - Centralino +39 011 6706111</p>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '            <div class="col l7 s12" >\n' +
+
+        '            </div>\n' +
+        '            <div class="col l7 s12" >\n' +
+        '                <p class="grey-text text-lighten-4">Progetto IUM-TWEB 2019/2020</p>\n' +
+        '            </div>\n' +
+        '            <div class="col l3 s12 hide-on-med-and-down"> \n'+
+        '                <h5 class="white-text"></h5>\n' +
+        '                <ul>\n' +
+        '                </ul>\n' +
+        '            </div>\n'+
+        '        </div>\n' +
+        '    </div>\n' +
+        '</footer>',
+});
